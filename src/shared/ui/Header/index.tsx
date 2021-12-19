07ec64e86a6bd365386851b80a8833d0cc123e09
sieugene <sieugene@mail.ru>
2021-12-19 13:55:51 +0300

    style : header

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,7 +1,6 @@
 import { css, jsx } from "@emotion/react";
 
 import styled from "@emotion/styled";
-import { Grid } from "@mui/material";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { Logo } from "shared/icons";
@@ -25,14 +24,8 @@ const Header = () => {
   ];
   return (
     <header>
-      <Grid
-        container
-        spacing={2}
-        xs={12}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Grid item>
+      <Wrapper>
+        <div>
           <Link href={ROUTES.home}>
             <a href={ROUTES.home}>
               <Header.LogoSection>
@@ -41,38 +34,40 @@ const Header = () => {
               </Header.LogoSection>
             </a>
           </Link>
-        </Grid>
+        </div>
 
         <Header.Links>
           {links?.map((link, index) => {
             return (
               <Link href={link.href} key={index}>
-                <a
-                  href={link.href}
-                  css={css`
-                    margin-right: 1rem;
-                    cursor: pointer;
-                    transition: 0.3s all ease;
-                    text-transform: capitalize;
-                    &:hover {
-                      opacity: 0.7;
-                    }
-                  `}
-                >
-                  {link.title}
-                </a>
+                <a href={link.href}>{link.title}</a>
               </Link>
             );
           })}
         </Header.Links>
-      </Grid>
+      </Wrapper>
     </header>
   );
 };
 
+const Wrapper = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.sm)} {
+    justify-content: center;
+  }
+`;
+
 Header.LogoSection = styled.div`
   display: flex;
   align-items: center;
+  margin-right: 10px;
+  ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.sm)} {
+    margin-top: 10px;
+  }
   h1 {
     font-weight: 400;
     font-size: 18px;
@@ -86,6 +81,24 @@ Header.StyledLogo = styled.div`
   background: black;
 `;
 
-Header.Links = styled(Grid)``;
+Header.Links = styled.div`
+  a {
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+
+    cursor: pointer;
+    transition: 0.3s all ease;
+    text-transform: capitalize;
+    color: ${(props) => props.theme.colors.grayish};
+    &:hover {
+      opacity: 0.7;
+      text-decoration: underline;
+    }
+  }
+  ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.sm)} {
+    margin-top: 10px;
+  }
+`;
 
 export default Header;
