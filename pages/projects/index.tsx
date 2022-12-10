fdebb954dc45cf727292a51cb352c6f96e860cc6
sieugene <sieugene@mail.ru>
2022-12-11 00:13:10 +0900

    chore : add new post

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,93 +1,107 @@
-import { css } from "@emotion/react";
-import styled from "@emotion/styled";
-import useTranslation from "next-translate/useTranslation";
-import Head from 'next/head';
-import { useState } from "react";
-import { projectsArray } from "shared/data/projects";
-import { BaseTypography, Card } from "shared/ui";
-import { CategoriesConstants } from "widgets/projects-categories/model/types";
+/* eslint-disable @next/next/no-html-link-for-pages */
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import useTranslation from 'next-translate/useTranslation'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useState } from 'react'
+import { projectsArray } from 'shared/data/projects'
+import { BaseButton, BaseTypography, Card } from 'shared/ui'
+import { CategoriesConstants } from 'widgets/projects-categories/model/types'
 
 const Projects = () => {
-  const { t } = useTranslation("common");
-  const [active, setActive] = useState(CategoriesConstants.Frontend);
+    const { t } = useTranslation('common')
+    const [active, setActive] = useState(CategoriesConstants.Frontend)
 
-  const projects = projectsArray;
+    const projects = projectsArray
 
-  return (
-    <>
-      <Head>
-        <title>Sieugene | Projects</title>
-      </Head>
-      <div
-        css={css`
-          margin-top: 25px;
-        `}
-      >
-        <BaseTypography
-          variant="h3"
-          css={css`
-            font-weight: 700;
-            color: cornflowerblue;
-          `}
-        >
-          {t("links.projects")}
-        </BaseTypography>
-        <BaseTypography
-          variant="caption"
-          css={css`
-            display: block;
-            white-space: pre-line;
-          `}
-        >
-          {t("projects.info")}
-        </BaseTypography>
-        <BaseTypography
-          variant="caption"
-          css={css`
-            display: block;
-            white-space: pre-line;
-            font-weight: bold;
-          `}
-        >
-          {t("projects.warning")}
-        </BaseTypography>
-        {/* <ProjectsCategories active={active} setActive={setActive} /> */}
-      </div>
+    return (
+        <>
+            <Head>
+                <title>Sieugene | Projects</title>
+            </Head>
+            <div
+                css={css`
+                    margin-top: 25px;
+                `}
+            >
+                <BaseTypography
+                    variant="h3"
+                    css={css`
+                        font-weight: 700;
+                        color: cornflowerblue;
+                    `}
+                >
+                    {t('links.projects')}
+                </BaseTypography>
+                <BaseTypography
+                    variant="caption"
+                    css={css`
+                        display: block;
+                        white-space: pre-line;
+                    `}
+                >
+                    {t('projects.info')}
+                </BaseTypography>
+                <BaseTypography
+                    variant="caption"
+                    css={css`
+                        display: block;
+                        white-space: pre-line;
+                        font-weight: bold;
+                    `}
+                >
+                    {t('projects.warning')}
+                </BaseTypography>
+                {/* <ProjectsCategories active={active} setActive={setActive} /> */}
 
-      <Cards>
-        {projects?.map((card) => (
-          <Card key={card.id} {...card} />
-        ))}
-      </Cards>
+                <BaseTypography
+                    variant="caption"
+                    css={css`
+                        color: blue;
+                        border: 1px solid;
+                        padding: 2px;
+                    `}
+                >
+                    {' '}
+                    <Link href="/post/pet-projects">More about it</Link>
+                </BaseTypography>
+            </div>
 
-      <h2>
-        {t("empty.check_on")}{" "}
-        <a
-          href="https://github.com/sieugene"
-          target="_blank"
-          rel="noreferrer"
-          css={css`
-            color: blue;
-          `}
-        >
-          {t("empty.git")}
-        </a>
-      </h2>
-    </>
-  );
-};
+            <Cards>
+                {projects?.map((card) => (
+                    <Card key={card.id} {...card} />
+                ))}
+            </Cards>
+
+            <h2>
+                {t('empty.check_on')}{' '}
+                <a
+                    href="https://github.com/sieugene"
+                    target="_blank"
+                    rel="noreferrer"
+                    css={css`
+                        color: blue;
+                    `}
+                >
+                    {t('empty.git')}
+                </a>
+            </h2>
+        </>
+    )
+}
 
 const Cards = styled.div`
-  margin-top: 25px;
-  margin-bottom: 50px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 300px);
-  grid-auto-rows: minmax(min-content, max-content);
-  grid-gap: 30px;
-  justify-content: space-between;
-  ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.md)} {
-    justify-content: center;
-  }
-`;
+    margin-top: 25px;
+    margin-bottom: 50px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 300px);
+    grid-auto-rows: minmax(min-content, max-content);
+    grid-gap: 30px;
+    justify-content: space-between;
+    ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.md)} {
+        justify-content: center;
+    }
+`
 
-export default Projects;
+export default Projects
--- src/entities/post/ui/Text/index.tsx
+++ src/entities/post/ui/Text/index.tsx
@@ -1,26 +1,28 @@
-import styled from "@emotion/styled";
-import { FC } from "react";
+import styled from '@emotion/styled'
+import { FC } from 'react'
 
 const Text: FC = ({ children }) => {
-  return <Paragraph>{children}</Paragraph>;
-};
+    return <Paragraph>{children}</Paragraph>
+}
 const Paragraph = styled.p`
-  margin-top: ${({ theme }) => theme.spaces.medium};
-  font-size: ${({ theme }) => theme.fontSize.text};
-  line-height: ${({ theme }) => theme.lineHeight.wide};
-  color: ${({ theme }) => theme.colors.black};
-  b {
-    font-weight: ${({ theme }) => theme.typography.fontWeightBold};
-  }
-  a {
-    color: ${({ theme }) => theme.colors.blue};
-    &:hover {
-      text-decoration: underline;
+    margin-top: ${({ theme }) => theme.spaces.medium};
+    font-size: ${({ theme }) => theme.fontSize.text};
+    line-height: ${({ theme }) => theme.lineHeight.wide};
+    color: ${({ theme }) => theme.colors.black};
+    b {
+        font-weight: ${({ theme }) => theme.typography.fontWeightBold};
     }
-  }
-  ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.sm)} {
-    font-size: ${({ theme }) => theme.fontSize.small};
-  }
-`;
+    a {
+        color: ${({ theme }) => theme.colors.blue};
+        margin-left: 4px;
+        margin-right: 4px;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    ${({ theme }) => theme.breakpoints.down(theme.breakpoints.values.sm)} {
+        font-size: ${({ theme }) => theme.fontSize.small};
+    }
+`
 
-export default Text;
+export default Text
