7057ca1dd11a166866bbb7193d156f44f076434e
“ぽっエフゲニー” <eugenepoluakov@gmail.com>
2023-04-30 21:14:03 +0900

    chore : Vercel アナリティック

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,48 @@
-import { ThemeProvider } from "@app";
-import { NextPage } from "next";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css";
-import { ReactElement, ReactNode } from "react";
-import { Layouts } from "shared/ui";
-import "../styles/globals.css";
+import { ThemeProvider } from '@app'
+import { Analytics } from '@vercel/analytics/react'
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'
+import { ReactElement, ReactNode } from 'react'
+import { Layouts } from 'shared/ui'
+import '../styles/globals.css'
 
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
-      </Head>
-      <ThemeProvider>
-        <Layouts withLayout={!Component.getLayout}>
-          {getLayout(<Component {...pageProps} />)}
-        </Layouts>
-      </ThemeProvider>
-    </>
-  );
+    getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp;
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
+
+    return (
+        <>
+            <Head>
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.png"
+                    type="image/x-icon"
+                />
+            </Head>
+            <ThemeProvider>
+                <Layouts withLayout={!Component.getLayout}>
+                    {getLayout(<Component {...pageProps} />)}
+                </Layouts>
+            </ThemeProvider>
+
+            <Analytics />
+        </>
+    )
+}
+
+export default MyApp
